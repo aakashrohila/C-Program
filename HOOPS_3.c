bf8204dd65cd827d216702898c3a625e2e6f0947
Aakash Rohila <53647802+aakashrohila@users.noreply.github.com>
2022-06-07 15:09:22 +0530

    CodeChef, Problem Code: HOOPS (AC)

---
+++ HOOPS_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int test_cases,hoops;
+	scanf("%d",&test_cases);
+	while(test_cases--)
+	{
+	    scanf("%d",&hoops);
+	    printf("%d\n",(hoops+1)/2);
+	 }
+	
+	
+	return 0;
+}
+
